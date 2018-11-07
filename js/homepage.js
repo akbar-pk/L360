37da493b4ae92e7bf18044bdb8ebbfed0bdd9424
akbar-pk <35654106+akbar-pk@users.noreply.github.com>
2018-11-07 16:51:12 +0530

    Add files via upload

--- js/homepage.js
+++ js/homepage.js
@@ -26,7 +26,7 @@ var homePage = (function(){
     //var supported = document.getElementById('support');
 
     // Listen for any clicks
-    shareButton.on('click', shareStoreInfo);
+    //shareButton.on('click', shareStoreInfo);
 
     function shareStoreInfo (ev) {
         alert("Share Test");
@@ -67,3 +67,33 @@ var homePage = (function(){
     }
 
 })();
+
+const shareBtn = document.querySelector('.share-store');
+const ogBtnContent = shareBtn.textContent;
+const title = document.querySelector('h1').textContent;
+const url = document.querySelector('link[rel=canonical]') &&
+            document.querySelector('link[rel=canonical]').href ||
+            window.location.href;
+
+shareBtn.addEventListener('click', () => {
+  if (navigator.share) {
+    navigator.share({
+      title,
+      url
+    }).then(() => {
+      showMessage(shareBtn, 'Thanks! 😄');
+    })
+    .catch(err => {
+      showMessage(shareBtn, `Couldn't share 🙁`);
+    });
+  } else {
+    showMessage(shareBtn, 'Not supported 🙅‍');
+  }
+});
+
+function showMessage(element, msg) {
+  element.textContent = msg;
+  setTimeout(() => {
+    element.textContent = ogBtnContent;
+  }, 2000);
+}
