0f9649df9f500b5a93d116759d21319a3ac0e21d
akbar-pk <35654106+akbar-pk@users.noreply.github.com>
2018-11-13 20:23:37 +0530

    Add files via upload

--- js/geolocation.js
+++ js/geolocation.js
@@ -4,18 +4,66 @@ window.onload = function(){
     if (navigator.geolocation) {
         console.log('Geolocation is supported');
         var startPos;
+        var modalTimeout;
+
+        var geoOptions = {
+            maximumAge: 5 * 60 * 1000,
+            timeout: 10 * 1000
+          }
         var geoSuccess = function(position) {
+            clearTimeout(modalTimeout);
             startPos = position;
-            document.getElementById('lat').innerHTML = startPos.coords.latitude;
-            document.getElementById('lon').innerHTML = startPos.coords.longitude;
+            
             console.log(startPos.coords.latitude);
             console.log(startPos.coords.longitude);
         };
-        navigator.geolocation.getCurrentPosition(geoSuccess);
+        var geoError = function(error) {
+            const $locationInfoModalHtml = $('#locationInfoModal');
+            let locationDialogOpts = {
+                backdrop: false, 
+                keyboard: false
+            };
+            
+            let showHideModal = function(display){
+                if(display == 'show') {
+                    modalTimeout = setTimeout(function(){
+                        $locationInfoModalHtml.modal(locationDialogOpts);
+                        $('.modal-backdrop').removeClass("modal-backdrop");
+                        //showHideModal('autoHide');
+                    }, 5000);
+                }
+                else if (display == 'hide') {
+                    $locationInfoModalHtml.modal('hide');
+                }
+                else if('autoHide') {
+                    setTimeout(function(){
+                        showHideModal('hide');
+                    }, 5000)
+                }
+            }
+            // close modal on click on close button
+            $locationInfoModalHtml.on('click', function(){
+                showHideModal('hide')
+            });
+            switch(error.code) {
+              case error.TIMEOUT:
+                // The user didn't accept the callout
+                showNudgeBanner();
+                console.log('Location servie not enabled');
+                //showHideModal('show');
+                break;
+
+            case 1:
+                console.log('User denied location');
+                showHideModal('show');
+                break;
+            }
+          };
+        navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
     }
     else {
-        document.getElementById('no_geo').innerHTML = "Location servie not enabled";
-        //console.log('Geolocation is not supported for this Browser/OS.');
+        
+        console.log('Geolocation is not supported for this Browser/OS.');
     }
 
 };
